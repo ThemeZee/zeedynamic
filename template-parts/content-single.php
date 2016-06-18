ac4459eb2faec4361bc200909126e3a9d346e448
Thomas W <netzberufler@googlemail.com>
2016-06-18 20:57:57 +0200

    improved coding of content template files and searchform

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -20,9 +20,14 @@
 	<?php zeedynamic_post_image_single(); ?>
 
 	<div class="entry-content clearfix">
+
 		<?php the_content(); ?>
-		<!-- <?php trackback_rdf(); ?> -->
-		<div class="page-links"><?php wp_link_pages(); ?></div>
+
+		<?php wp_link_pages( array(
+			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'zeedynamic' ),
+			'after'  => '</div>',
+		) ); ?>
+
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
