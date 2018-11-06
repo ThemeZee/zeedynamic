25f52225be108014b86075549933b8ae42f7da04
Thomas Weichselbaumer <kontakt@themecoder.de>
2018-11-06 17:36:46 +0100

    added styling and color palette for Gutenberg

--- functions.php
+++ functions.php
@@ -6,10 +6,11 @@
  */
 
 /**
- * The zeeDynamic theme only works in WordPress 4.4 or later.
+ * zeeDynamic only works in WordPress 4.7 or later.
  */
-if ( version_compare( $GLOBALS['wp_version'], '4.4-alpha', '<' ) ) {
+if ( version_compare( $GLOBALS['wp_version'], '4.7-alpha', '<' ) ) {
 	require get_template_directory() . '/inc/back-compat.php';
+	return;
 }
 
 
@@ -54,17 +55,17 @@ if ( ! function_exists( 'zeedynamic_setup' ) ) :
 
 		// Set up the WordPress core custom logo feature.
 		add_theme_support( 'custom-logo', apply_filters( 'zeedynamic_custom_logo_args', array(
-			'height' => 40,
-			'width' => 250,
+			'height'      => 40,
+			'width'       => 250,
 			'flex-height' => true,
-			'flex-width' => true,
+			'flex-width'  => true,
 		) ) );
 
 		// Set up the WordPress core custom header feature.
-		add_theme_support('custom-header', apply_filters( 'zeedynamic_custom_header_args', array(
+		add_theme_support( 'custom-header', apply_filters( 'zeedynamic_custom_header_args', array(
 			'header-text' => false,
-			'width'	=> 1230,
-			'height' => 410,
+			'width'       => 1230,
+			'height'      => 410,
 			'flex-height' => true,
 		) ) );
 
@@ -77,6 +78,34 @@ if ( ! function_exists( 'zeedynamic_setup' ) ) :
 		// Add Theme Support for Selective Refresh in Customizer.
 		add_theme_support( 'customize-selective-refresh-widgets' );
 
+		// Add custom color palette for Gutenberg.
+		add_theme_support( 'editor-color-palette', array(
+			array(
+				'name'  => esc_html_x( 'Primary', 'Gutenberg Color Palette', 'zeedynamic' ),
+				'slug'  => 'primary',
+				'color' => apply_filters( 'zeedynamic_primary_color', '#e84747' ),
+			),
+			array(
+				'name'  => esc_html_x( 'White', 'Gutenberg Color Palette', 'zeedynamic' ),
+				'slug'  => 'white',
+				'color' => '#ffffff',
+			),
+			array(
+				'name'  => esc_html_x( 'Light Gray', 'Gutenberg Color Palette', 'zeedynamic' ),
+				'slug'  => 'light-gray',
+				'color' => '#f0f0f0',
+			),
+			array(
+				'name'  => esc_html_x( 'Dark Gray', 'Gutenberg Color Palette', 'zeedynamic' ),
+				'slug'  => 'dark-gray',
+				'color' => '#777777',
+			),
+			array(
+				'name'  => esc_html_x( 'Black', 'Gutenberg Color Palette', 'zeedynamic' ),
+				'slug'  => 'black',
+				'color' => '#333333',
+			),
+		) );
 	}
 endif;
 add_action( 'after_setup_theme', 'zeedynamic_setup' );
@@ -102,35 +131,34 @@ add_action( 'after_setup_theme', 'zeedynamic_content_width', 0 );
 function zeedynamic_widgets_init() {
 
 	register_sidebar( array(
-		'name' => esc_html__( 'Sidebar', 'zeedynamic' ),
-		'id' => 'sidebar',
-		'description' => esc_html__( 'Appears on posts and pages except the full width template.', 'zeedynamic' ),
+		'name'          => esc_html__( 'Sidebar', 'zeedynamic' ),
+		'id'            => 'sidebar',
+		'description'   => esc_html__( 'Appears on posts and pages except the full width template.', 'zeedynamic' ),
 		'before_widget' => '<aside id="%1$s" class="widget %2$s clearfix">',
-		'after_widget' => '</aside>',
-		'before_title' => '<div class="widget-header"><h3 class="widget-title">',
-		'after_title' => '</h3></div>',
-	));
+		'after_widget'  => '</aside>',
+		'before_title'  => '<div class="widget-header"><h3 class="widget-title">',
+		'after_title'   => '</h3></div>',
+	) );
 
 	register_sidebar( array(
-		'name' => esc_html__( 'Header', 'zeedynamic' ),
-		'id' => 'header',
-		'description' => esc_html__( 'Appears on header area. You can use a search or ad widget here.', 'zeedynamic' ),
+		'name'          => esc_html__( 'Header', 'zeedynamic' ),
+		'id'            => 'header',
+		'description'   => esc_html__( 'Appears on header area. You can use a search or ad widget here.', 'zeedynamic' ),
 		'before_widget' => '<aside id="%1$s" class="header-widget %2$s">',
-		'after_widget' => '</aside>',
-		'before_title' => '<h4 class="header-widget-title">',
-		'after_title' => '</h4>',
-	));
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h4 class="header-widget-title">',
+		'after_title'   => '</h4>',
+	) );
 
 	register_sidebar( array(
-		'name' => esc_html__( 'Magazine Homepage', 'zeedynamic' ),
-		'id' => 'magazine-homepage',
-		'description' => esc_html__( 'Appears on blog index and Magazine Homepage template. You can use the Magazine widgets here.', 'zeedynamic' ),
+		'name'          => esc_html__( 'Magazine Homepage', 'zeedynamic' ),
+		'id'            => 'magazine-homepage',
+		'description'   => esc_html__( 'Appears on blog index and Magazine Homepage template. You can use the Magazine widgets here.', 'zeedynamic' ),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-		'after_widget' => '</div>',
-		'before_title' => '<div class="widget-header"><h3 class="widget-title">',
-		'after_title' => '</h3></div>',
-	));
-
+		'after_widget'  => '</div>',
+		'before_title'  => '<div class="widget-header"><h3 class="widget-title">',
+		'after_title'   => '</h3></div>',
+	) );
 }
 add_action( 'widgets_init', 'zeedynamic_widgets_init' );
 
@@ -163,7 +191,6 @@ function zeedynamic_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
-
 }
 add_action( 'wp_enqueue_scripts', 'zeedynamic_scripts' );
 
@@ -172,12 +199,19 @@ add_action( 'wp_enqueue_scripts', 'zeedynamic_scripts' );
  * Enqueue custom fonts.
  */
 function zeedynamic_custom_fonts() {
-
-	// Register and Enqueue Theme Fonts.
 	wp_enqueue_style( 'zeedynamic-custom-fonts', get_template_directory_uri() . '/assets/css/custom-fonts.css', array(), '20180413' );
-
 }
 add_action( 'wp_enqueue_scripts', 'zeedynamic_custom_fonts', 1 );
+add_action( 'enqueue_block_editor_assets', 'zeedynamic_custom_fonts', 1 );
+
+
+/**
+ * Enqueue editor styles for the new Gutenberg Editor.
+ */
+function zeedynamic_block_editor_assets() {
+	wp_enqueue_style( 'zeedynamic-editor-styles', get_theme_file_uri( '/assets/css/gutenberg-styles.css' ), array(), '20181102', 'all' );
+}
+add_action( 'enqueue_block_editor_assets', 'zeedynamic_block_editor_assets' );
 
 
 /**
@@ -195,7 +229,6 @@ function zeedynamic_add_image_sizes() {
 	add_image_size( 'zeedynamic-thumbnail-small', 100, 80, true );
 	add_image_size( 'zeedynamic-thumbnail-medium', 350, 230, true );
 	add_image_size( 'zeedynamic-thumbnail-large', 420, 280, true );
-
 }
 add_action( 'after_setup_theme', 'zeedynamic_add_image_sizes' );
 
--- inc/back-compat.php
+++ inc/back-compat.php
@@ -2,9 +2,9 @@
 /**
  * zeeDynamic back compat functionality
  *
- * Prevents zeeDynamic from running on WordPress versions prior to 4.4,
+ * Prevents zeeDynamic from running on WordPress versions prior to 4.7,
  * since this theme is not meant to be backward compatible beyond that and
- * relies on many newer functions and markup changes introduced in 4.4.
+ * relies on many newer functions and markup changes introduced in 4.7.
  *
  * @package zeeDynamic
  *
@@ -33,35 +33,35 @@ add_action( 'after_switch_theme', 'zeedynamic_switch_theme' );
  * Adds a message for unsuccessful theme switch.
  *
  * Prints an update nag after an unsuccessful attempt to switch to
- * zeeDynamic on WordPress versions prior to 4.4.
+ * zeeDynamic on WordPress versions prior to 4.7.
  *
  * @global string $wp_version WordPress version.
  */
 function zeedynamic_upgrade_notice() {
-	$message = sprintf( esc_html__( '%1$s requires at least WordPress version %2$s. You are running version %3$s. Please upgrade and try again.', 'zeedynamic' ), 'zeeDynamic', '4.4', $GLOBALS['wp_version'] );
+	$message = sprintf( esc_html__( '%1$s requires at least WordPress version %2$s. You are running version %3$s. Please upgrade and try again.', 'zeedynamic' ), 'zeeDynamic', '4.7', $GLOBALS['wp_version'] );
 	printf( '<div class="error"><p>%s</p></div>', $message );
 }
 
 /**
- * Prevents the Customizer from being loaded on WordPress versions prior to 4.4.
+ * Prevents the Customizer from being loaded on WordPress versions prior to 4.7.
  *
  * @global string $wp_version WordPress version.
  */
 function zeedynamic_customize() {
-	wp_die( sprintf( esc_html__( '%1$s requires at least WordPress version %2$s. You are running version %3$s. Please upgrade and try again.', 'zeedynamic' ), 'zeeDynamic', '4.4', $GLOBALS['wp_version'] ), '', array(
+	wp_die( sprintf( esc_html__( '%1$s requires at least WordPress version %2$s. You are running version %3$s. Please upgrade and try again.', 'zeedynamic' ), 'zeeDynamic', '4.7', $GLOBALS['wp_version'] ), '', array(
 		'back_link' => true,
 	) );
 }
 add_action( 'load-customize.php', 'zeedynamic_customize' );
 
 /**
- * Prevents the Theme Preview from being loaded on WordPress versions prior to 4.4.
+ * Prevents the Theme Preview from being loaded on WordPress versions prior to 4.7.
  *
  * @global string $wp_version WordPress version.
  */
 function zeedynamic_preview() {
 	if ( isset( $_GET['preview'] ) ) {
-		wp_die( sprintf( esc_html__( '%1$s requires at least WordPress version %2$s. You are running version %3$s. Please upgrade and try again.', 'zeedynamic' ), 'zeeDynamic', '4.4', $GLOBALS['wp_version'] ) );
+		wp_die( sprintf( esc_html__( '%1$s requires at least WordPress version %2$s. You are running version %3$s. Please upgrade and try again.', 'zeedynamic' ), 'zeeDynamic', '4.7', $GLOBALS['wp_version'] ) );
 	}
 }
 add_action( 'template_redirect', 'zeedynamic_preview' );
